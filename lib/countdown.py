c0962040571000b334d8b603d7d72f3055953ba0
Luck Tevin <luck.tevin@student.moringaschool.com>
2024-03-03 21:30:09 +0300

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+def countdown(n):
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        n -= 1
+    print("HAPPY NEW YEAR!")
+#countdown_with_sleep()
+import time
+
+def countdown_with_sleep(n):
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        n -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
